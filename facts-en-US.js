e18fc7b8c4cc6ca6064831e126fbac830f1d22da
Mahima Gupta <mahimagupta2795@gmail.com>
2018-07-26 01:11:21 -0700

    Facts Data

---
+++ facts-en-US.js
@@ -0,0 +1,18 @@
+var data = [
+"The University of Southern California Mascot is not Tommy Trojan; it is actually Traveler the Horse.",
+"USC is the only university in the world to have a gold medal-winning athlete in every summer Olympiad since 1912.",
+"USC’s “Fight On” song was used to inspire combat-bound troops in the Aleutians Campaign during World War II. The song was written by Milo Sweet, a dental student.",
+"USC has more than 3,000 children in college access programs and more than 500 children in pre-school programs.",
+"USC students, staff and faculty devote more than 650,000 hours in the community",
+"A “Perfect Day” is when USC wins their football game and both UCLA and Notre Dame both lose. There have been 35 “Perfect Days” to date.",
+"USC has adopted 15 neighborhood elementary, middle and high schools as part of its Family of Schools program, sharing with these schools its students and educational resources.",
+"And the only university to have an alum nominated for an Oscar every year since the Academy Awards’ inception in 1929. Ron Howard and Brian Grazer were classmates at USC.",
+"The southwest corner of the Pueblo of Los Angeles, the original land grant given by the King of Spain in 1781, is at Figueroa Street and Exposition Boulevard (across the street from USC).",
+"When USC first opened, tuition was $15.00 per term, and students were not allowed to leave town without the knowledge and consent of the university president. This is what the neighborhood next to USC used to look like when it opened in the 1880s."
+];
+
+var SKILL_NAME = "USC Facts";
+var GET_FACT_MESSAGE = "Here's your fact: ";
+var HELP_MESSAGE = "You can say tell me a usc fact, or, you can say exit... What can I help you with?";
+var HELP_REPROMPT = "What can I help you with?";
+var STOP_MESSAGE = "Goodbye!";
